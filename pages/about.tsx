7e246929c504dab4e1dd9236f272f104897a1e0c
sydneyhopp <sydneyhoppenworth@gmail.com>
2025-01-27 14:16:35 -0800

    nav bar

---
+++ pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+
+    return (
+
+        <div>
+            me n laura n loki in sf for a quarter yay
+        </div>
+
+    )
+
+};
---
+++ pages/best.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Best() {
+
+    return (
+
+        <div>
+            welcome to the best of the best mf
+        </div>
+
+    )
+
+}
