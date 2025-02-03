cb96dac4997938e7ef2d9e0939d41162219acc2a
sydneyhopp <sydneyhoppenworth@gmail.com>
2025-02-03 13:57:47 -0800

    try

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import React from 'react';
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- pages/latest.tsx
+++ pages/latest.tsx
@@ -1,14 +1,74 @@
-import React from "react";
+/* eslint-disable */
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import { API_URL } from "@/utils/constants";
+
+const axiosInstance = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+// use axios to do a GET request to grab posts
+
+interface Data{
+    title: string;
+    text: string;
+    location: string;
+    subheading: string;
+    author: string;
+    date: [];
+}
 
 export default function Latest() {
+
+    const [data, setData] = useState<Data[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>();
+
+    const fetchData = async() => {
+        try {
+            const response = await axiosInstance.get("/grab-posts");
+            if (response.data && Array.isArray(response.data.data)){
+                setData(response.data.data);
+            } else {
+                throw new Error("Invalid data format");
+            }
+            
+        } catch (error) {
+            setError("Failed to load posts.");
+        } finally {
+            setLoading(false);
+        }
+    }
+    
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+    if (loading) return <p>Loading latest posts...</p>;
+    if (error) return <p style={{ color: "red" }}>{error}</p>;
 
     return (
 
         <div>
-            wassup wassup wassuppppppp...
-            here&apos;s the latest from me n laura
+            <h2>Latest from Me & Laura</h2>
+            <p>wassup wassup wassuppppppp... here&apos;s the latest from me n laura</p>
+            <ul>   
+                {data.length > 0 ? (
+                    data.map((post, index) => (
+                        <li key={index}>
+                            <h2>{post.title}</h2>
+                            <h3>{post.location}</h3>
+                            <p>author: {post.author}, {post.date[3]}/{post.date[2]}/{post.date[4]}</p>
+                            <p>{post.text}</p>
+                        </li>
+                    ))
+                ) : (
+                    <p>No posts available.</p>
+                )}
+            </ul>
         </div>
-
-    )
-
+    );
 };
