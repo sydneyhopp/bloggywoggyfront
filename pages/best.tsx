1889a9aade85aa261b7f0bf8865d10de20ce7136
laurasfelix <laurasfelix@hotmail.com.br>
2025-02-24 13:00:59 -0800

    updated navbar for logo + index ui

--- pages/best.tsx
+++ pages/best.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Navbar from "@/components/navbar";
 
 export default function Best() {
 
     return (
-
+        
         <div>
+            <Navbar/>
             welcome to the best of the best mf
         </div>
 
